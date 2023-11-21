c3811b5b79794423c2a67ff4a817a2aa52c29d9e
Kain Nhantumbo <nhantumbok@gmail.com>
2023-11-21 16:12:24 +0200

    feat: first commit

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,29 @@
+import ThemeContext from './ThemeContext';
+import { ReactNode, useEffect } from 'react';
+import { updateSizes } from '@/state/slices/innerWindowSizeSlice';
+import { useDispatch } from 'react-redux';
+
+type Props = { children: ReactNode };
+
+export default function AppContext({ children }: Props) {
+  const dispatch = useDispatch();
+
+  const computeInnerWindowSize = (): void => {
+    dispatch(
+      updateSizes({
+        width: Number(window.innerWidth.toFixed(0)),
+        height: Number(window.innerHeight.toFixed(0))
+      })
+    );
+  };
+
+  useEffect(() => {
+    computeInnerWindowSize();
+    window.addEventListener('resize', computeInnerWindowSize);
+    return () => {
+      window.removeEventListener('resize', computeInnerWindowSize);
+    };
+  }, []);
+
+  return <ThemeContext>{children}</ThemeContext>;
+}
